c0a411378ab7c3c8c7851042f0e441aa8c800a6a
Aayush <aayushsoni424@gmail.com>
2024-04-25 21:34:27 +0530

    Update movie route to include movie details and trailer functionality

--- app/routes/movie.py
+++ app/routes/movie.py
@@ -1,8 +1,19 @@
-from flask import render_template
+from flask import render_template, jsonify
 from app.routes import movie_bp
+from datetime import datetime
+from app.utils.helper import movie_response, get_movie_id_by_name, get_movie_trailer
+
 
 
 @movie_bp.route("/<path:movie_name>")
 def movie(movie_name):
-    original_movie_name = movie_name.replace("-", " ")
-    return render_template("movie.html", movie_name=original_movie_name)
+    movie_id = get_movie_id_by_name(movie_name)
+    movie = movie_response(movie_id=movie_id)
+    movie['release'] = datetime.strptime(movie['release_date'], "%Y-%m-%d").strftime("%d %B %Y")
+    movie['trailer'] = get_movie_trailer(movie_id)
+    return render_template("movie.html", movie=movie)
+
+
+@movie_bp.route("/<int:movie_id>")
+def movie_detail(movie_id):
+    return jsonify(movie_response(movie_id))
