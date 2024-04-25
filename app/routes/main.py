7e2ffa14688c27e12061c2a371641e57498c410c
Aayush <aayushsoni424@gmail.com>
2024-04-25 21:33:30 +0530

    Add release year to movie data in main.py

--- app/routes/main.py
+++ app/routes/main.py
@@ -6,4 +6,8 @@ from app.utils.helper import popular_movies
 @main_bp.route("/")
 def index():
     movies = popular_movies()
+    for movie in movies:
+        release_date = movie.get("release_date", "")
+        release_year = release_date.split("-")[0] if release_date else ""
+        movie["release_year"] = release_year
     return render_template("index.html", movies=movies)
