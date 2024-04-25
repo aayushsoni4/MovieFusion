cbb354527f2628f8017c6a141b5bd87216d824ea
Aayush <aayushsoni424@gmail.com>
2024-04-25 16:12:34 +0530

    Create Movie page and Add routes

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,8 +1,12 @@
 from app.routes import main_bp
-from flask import render_template, url_for
+from flask import render_template, url_for, request
 
 
 @main_bp.route("/")
 def index():
-    title = "MovieFusion"
-    return render_template("index.html", title=title)
+    return render_template("index.html")
+
+
+@main_bp.route("/movie/<movie_name>")
+def movie(movie_name):
+    return render_template("movie.html", movie_name=movie_name)
