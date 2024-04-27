02882392a6128ca1a0258cd93d8c3fedec2a2a22
Aayush <aayushsoni424@gmail.com>
2024-04-28 04:43:28 +0530

    Add new routes for forgot password and registration in auth.py

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -4,3 +4,13 @@ from app.routes import auth_bp
 @auth_bp.route("/login")
 def login():
     return "<h1>Login</h1>"
+
+
+@auth_bp.route("/forgot_password")
+def forgot_password():
+    return "<h1>Forgot Password</h1>"
+
+
+@auth_bp.route("/register")
+def register():
+    return "<h1>Register</h1>"
