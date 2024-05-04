ba4c0a8345ac99b7241a683905bf87d6f636c602
Aayush <aayushsoni424@gmail.com>
2024-05-04 23:18:33 +0530

    Update login and register routes to handle form submissions and render templates

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,16 +1,21 @@
 from app.routes import auth_bp
+from flask import render_template, request, url_for, redirect
 
 
-@auth_bp.route("/login")
+@auth_bp.route("/login", methods=["POST", "GET"])
 def login():
-    return "<h1>Login</h1>"
+    if request.method == "POST":
+        return redirect(url_for("main.index"))
+    return render_template("login.html")
 
 
-@auth_bp.route("/forgot_password")
+@auth_bp.route("/forgot_password", methods=["POST", "GET"])
 def forgot_password():
     return "<h1>Forgot Password</h1>"
 
 
-@auth_bp.route("/register")
+@auth_bp.route("/register", methods=["POST", "GET"])
 def register():
-    return "<h1>Register</h1>"
+    if request.method == "POST":
+        return redirect(url_for("main.index"))
+    return render_template("register.html")
