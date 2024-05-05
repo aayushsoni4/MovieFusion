5b0ffeaeb9a16e4144e30e926c73ebfde927f2f2
Aayush <aayushsoni424@gmail.com>
2024-05-05 23:16:15 +0530

    Add app.log to .gitignore and update Flask initialization in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,26 +4,39 @@ from flask_login import LoginManager
 from config import Config
 from logger import logger
 
-
+# Initialize Flask extensions
 db = SQLAlchemy()
 login_manager = LoginManager()
 
 
 def create_app(config_class=Config):
+    """
+    Create and configure the Flask application.
+
+    Args:
+        config_class (class): Configuration class for the Flask application.
+
+    Returns:
+        Flask: Initialized Flask application.
+    """
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    # Register custom Jinja filter for URL slugs
     from app.utils.helper import url_slug
 
     app.jinja_env.filters["url_slug"] = url_slug
 
+    # Initialize Flask extensions
     db.init_app(app)
     login_manager.init_app(app)
 
+    # Handle unauthorized access
     @login_manager.unauthorized_handler
     def unauthorized_callback():
         return redirect(url_for("auth.login"))
 
+    # Register blueprints for different routes
     from app.routes import auth_bp, main_bp, movie_bp, category_bp, search_bp
 
     app.register_blueprint(auth_bp, url_prefix="/auth")
@@ -32,9 +45,11 @@ def create_app(config_class=Config):
     app.register_blueprint(search_bp, url_prefix="/search")
     app.register_blueprint(main_bp)
 
+    # Create database tables
     with app.app_context():
         db.create_all()
 
+    # Load user for Flask-Login
     @login_manager.user_loader
     def load_user(user_id):
         from app.models import User
--- config.py
+++ config.py
@@ -1,12 +1,21 @@
 import os
 from dotenv import load_dotenv
 
+# Load environment variables from .env file
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 load_dotenv()
 
 
 class Config:
+    """
+    Configuration settings for the Flask application.
+
+    Attributes:
+        SECRET_KEY (str): Secret key for protecting against CSRF attacks.
+        SQLALCHEMY_DATABASE_URI (str): Database URI for connecting to the SQLite database.
+        SQLALCHEMY_TRACK_MODIFICATIONS (bool): Flag to track modifications in the database.
+    """
+
     SECRET_KEY = os.getenv("FLASK_SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "movies.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
