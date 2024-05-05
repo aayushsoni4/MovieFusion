60df1b5bd974451e5197fbf0da8d17c257552064
Aayush <aayushsoni424@gmail.com>
2024-05-05 20:17:05 +0530

    Update logger.py to limit log file size to 20 KB

--- logger.py
+++ logger.py
@@ -22,7 +22,7 @@ logger.setLevel(log_level)
 # Create handlers
 console_handler = logging.StreamHandler()
 file_handler = RotatingFileHandler(
-    "app.log", maxBytes=1024 * 1024 * 5, backupCount=10  # 5 MB
+    "app.log", maxBytes=1024 * 20, backupCount=10  # 20 KB
 )
 
 # Create formatters and add it to handlers
